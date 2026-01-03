53c75b85c4e79cc2ef6f53f68d427e1e6c864d94
imranahmetov-png <imranahmetov28@gmail.com>
2026-01-03 23:13:11 +0600

    Подключил навигационное меню

--- src/App.tsx
+++ src/App.tsx
@@ -9,20 +9,32 @@ function App() {
     <div className="bg-[#121212] min-w-screen min-h-screen pt-[57px] sm:min-w-screen overflow-x-hidden sm:min-h-screen">
       <div className="flex flex-col justify-between items-center gap-[150px]">
         {/* UI HERO SECTION */}
-        <div className="flex flex-col w-full justify-between items-center gap-[150px] p-4">
+        <div
+          id="Home"
+          className="flex flex-col w-full justify-between items-center gap-[150px] p-4"
+        >
           <UIHeader />
           <UiHero />
         </div>
         {/*Services Section */}
-        <div className="flex flex-col justify-start items-center gap-[150px] p-4">
+        <div
+          id="Services"
+          className="flex flex-col justify-start items-center gap-[150px] p-4"
+        >
           <UiFrame />
         </div>
         {/* About Me Section */}
-        <div className="flex flex-col justify-start items-center gap-[150px] p-4">
+        <div
+          id="About me"
+          className="flex flex-col justify-start items-center gap-[150px] p-4"
+        >
           <UiAboutMe />
         </div>
         {/* Portfolio Section */}
-        <div className="flex flex-col justify-center items-center gap-[150px] p-4">
+        <div
+          id="Portfolio"
+          className="flex flex-col justify-center items-center gap-[150px] p-4"
+        >
           <UiPortfolio />
         </div>
       </div>
